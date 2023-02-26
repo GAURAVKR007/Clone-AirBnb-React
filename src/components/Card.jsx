1a5765cfa41676d5f8d82c0b1f8a10bd2f0beb5e
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-02-26 19:02:47 +0530

    bnb clone complete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,31 @@ import React from "react";
 // import cardImg from "../Images/mclaren.jpg"
 
 function Card(props) {
+
+    let bageText
+    if(props.openSpots === 0){
+        bageText = "SOLD OUT"
+    }else if(props.location === "Online") {
+        bageText = "ONLINE"
+    }else{
+        bageText = ""
+    }
+
     return(
         <div className="card">
+        { bageText && <div className="card--badge">{bageText}</div> }
             <img className="card-img" 
             alt="card-img" 
-            src={props.img}
+            src={props.item.coverImg}
               />
             <div className="card-stats">
             <i class="fa-solid fa-star card-star"></i>
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewCount}) </span>
-            <span className="gray">-{'>'} {props.location}</span>
+            <span>{props.item.stats.rating}</span>
+            <span className="gray">({props.item.stats.reviewCount}) </span>
+            <span className="gray">-{'>'} {props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / car</p>
+            <p>{props.item.title}</p>
+            <p><span className="bold">From ${props.item.price}</span> / car</p>
         </div>
     )
 }
