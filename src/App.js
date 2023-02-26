e65bc4865be46140e41104a02b6180d3c4e9f78e
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-02-26 18:31:02 +0530

    Added .map() function to map the card element

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import './style.css'
 import Navbar from './components/Navbar';
 import  Hero from './components/Hero'
 import Card from './components/Card';
+import data from './data';
+
+
 
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id}
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-        img = "../images/mclaren.jpg"
-        rating = "5.0"
-        reviewCount = "7"
-        country="USA"
-        title="Life Lesson with Mclern"
-        price="2B"
-      />
+      <section className='cards-list'>{cards}</section>
+      
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ function Card(props) {
             <i class="fa-solid fa-star card-star"></i>
             <span>{props.rating}</span>
             <span className="gray">({props.reviewCount}) </span>
-            <span className="gray">-{'>'} {props.country}</span>
+            <span className="gray">-{'>'} {props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / car</p>
